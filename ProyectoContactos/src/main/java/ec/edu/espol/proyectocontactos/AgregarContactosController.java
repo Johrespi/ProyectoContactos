1ff020d464b45f77e88597b21eefae49b66c7710
Johrespi <johrespi@espol.edu.ec>
2023-11-25 03:26:28 -0500

    asd

--- ProyectoContactos/src/main/java/ec/edu/espol/proyectocontactos/AgregarContactosController.java
+++ ProyectoContactos/src/main/java/ec/edu/espol/proyectocontactos/AgregarContactosController.java
@@ -6,7 +6,10 @@ package ec.edu.espol.proyectocontactos;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -14,6 +17,57 @@ import javafx.fxml.Initializable;
  * @author johan
  */
 public class AgregarContactosController implements Initializable {
+
+    @FXML
+    private Label tituloLabel;
+    @FXML
+    private Label nombreLabel;
+    @FXML
+    private Label apellidoLabel;
+    @FXML
+    private Label emailField;
+    @FXML
+    private Label empresaLabel;
+    @FXML
+    private Label actualizadorLabel;
+    @FXML
+    private Button crearButton;
+    @FXML
+    private Button addFechaButton;
+    @FXML
+    private Label tipoFechaLabel;
+    @FXML
+    private Label fechaLabel;
+    @FXML
+    private Button addFotoButton;
+    @FXML
+    private Label tipoRelacionLabel;
+    @FXML
+    private Label nombreRelacionLabel;
+    @FXML
+    private Button addRelacionButton;
+    @FXML
+    private Label EtiquetaEmailLabel;
+    @FXML
+    private Button addEmailButton;
+    @FXML
+    private Label EtiquetaDireccionLabel;
+    @FXML
+    private Button addDireccionButton;
+    @FXML
+    private Label direccionLabel;
+    @FXML
+    private Label etiquetaTelLabel;
+    @FXML
+    private Label telefonoLabel;
+    @FXML
+    private Button addTelefonoButton;
+    @FXML
+    private Label redSocialLabel;
+    @FXML
+    private Label idRedSocialLabel;
+    @FXML
+    private Button addRedSocialButton;
 
     /**
      * Initializes the controller class.
